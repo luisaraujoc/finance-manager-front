efd558e0d51bc13622a473aeb69c74dfbc3d42f9
luisaraujoc <luislk_@outlook.com>
2025-03-10 21:11:51 -0300

    index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const user = JSON.parse(localStorage.getItem("usuario"));
 
 btnLogIn = document.getElementById("logIn");
@@ -17,26 +16,4 @@ btnSignUp.addEventListener("click", () => {
   localStorage.clear();
   user = null;
   window.location.href = "http://localhost/finance-manager-front/signup.html";
-=======
-document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
-    const mobileMenu = document.querySelector('.mobile-menu');
-
-    mobileMenuIcon.addEventListener('click', function() {
-        mobileMenu.classList.toggle('active');
-    });
-
-    document.addEventListener('click', function(event) {
-        if (!event.target.closest('.header-content-right') && 
-            !event.target.closest('.mobile-menu')) {
-            mobileMenu.classList.remove('active');
-        }
-    });
-
-    mobileMenu.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', () => {
-            mobileMenu.classList.remove('active');
-        });
-    });
->>>>>>> 979c13d241272a29e5f07605ed282e67652afd05
 });
